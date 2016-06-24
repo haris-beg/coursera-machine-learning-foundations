c1a30d65ff49e09e308d393a599c9f8d7abaabfc
harisbeg <haris.beg@hotmail.com>
2016-06-23 18:38:36 -0700

    Conditional statements and loops in Python 2.7

---
+++ week-1/conditional-statements-loops.py
@@ -0,0 +1,21 @@
+i = 4
+f = 4.23
+
+# if else statement
+if i == 1 and f > 4:
+    print "The value of i is 1 and f is greater than 4."
+elif i > 4 or f > 4:
+    print "either i or f or both are greater than 4"
+else:
+    print "both i and f are less than or equal to 4"
+
+# for loop
+l = [3, 2, 1]
+for e in l:
+    print e
+    
+# while loop
+counter = 1
+while counter < 10:
+    print counter
+    counter += 1
